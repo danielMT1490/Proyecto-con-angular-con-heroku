33c8d49366fd235679045d8b9ef1694dc4d3b2f0
Daniel Madrigal <danieltristan1991@gmail.com>
2018-05-23 10:32:16 +0200

    updates y subir a heroku

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-//server.js
-const express = require ('express');
-const app = express ();
-const path = require ('ruta');
-app.use (express.static ('./ dist / helloworld'));
-app.get ('/ *', función (req, res) {
-  res.sendFile (path.join ('./ dist / helloworld / index.html'));
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/helloworld'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/helloworld/index.html'));
 });
-app.listen (process.env.PORT || 8080);
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
